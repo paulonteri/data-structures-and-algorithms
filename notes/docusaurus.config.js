52d2f2ba1c560160a605883ffebba4425c988e4b
Paul Onteri <me@paulonteri.com>
2022-09-04 11:21:31 +0300

    fix url

--- notes/docusaurus.config.js
+++ notes/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
     title: "DSA | PaulOnteri.com",
     tagline: "DSA & System Design",
-    url: "https://paulontteri.com",
+    url: "https://paulonteri.com",
     baseUrl: "/dsa/",
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
