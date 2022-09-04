c1086a34d01ea5a1be0e5055c31ff8664bec983f
Paul Onteri <me@paulonteri.com>
2022-09-04 12:49:52 +0300

    disable search

--- notes/docusaurus.config.js
+++ notes/docusaurus.config.js
@@ -136,16 +136,16 @@ const config = {
                 darkTheme: darkCodeTheme,
             },
         }),
-    themes: [
-        [
-            require.resolve("@easyops-cn/docusaurus-search-local"),
-            {
-                // ... Your options.
-                // `hashed` is recommended as long-term-cache of index file is possible.
-                hashed: true,
-            },
-        ],
-    ],
+    // themes: [
+    //     [
+    //         require.resolve("@easyops-cn/docusaurus-search-local"),
+    //         {
+    //             // ... Your options.
+    //             // `hashed` is recommended as long-term-cache of index file is possible.
+    //             hashed: true,
+    //         },
+    //     ],
+    // ],
 };
 
 module.exports = config;
